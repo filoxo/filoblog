5be72b13294e30568a73d3925446389b4d4607fd
Carlos Filoteo <c.filoteo12@gmail.com>
2020-04-24 10:33:48 -0600

    Prevent theme flashing by rendering nothing in SSR

--- src/components/theme-toggle.js
+++ src/components/theme-toggle.js
@@ -2,28 +2,30 @@ import React, { useState, useEffect } from 'react'
 import styles from './theme-toggle.module.css'
 
 const THEME = 'dark-theme-enabled'
+const isSSR = typeof window === 'undefined'
 
-export default function ThemeToggle(props) {
-  const [darkModeEnabled, setDarkModeEnabled] = useState(false)
+const ThemeToggleComponent = isSSR
+  ? () => null
+  : function ThemeToggle(props) {
+      const [darkModeEnabled, setDarkModeEnabled] = useState(
+        () => localStorage.getItem(THEME) === 'true'
+      )
 
-  useEffect(() => {
-    // Have to do this here instead of useState because of Gatbsy ssr
-    setDarkModeEnabled(localStorage.getItem(THEME) === 'true')
-  }, [])
+      useEffect(() => {
+        localStorage.setItem(THEME, darkModeEnabled)
+        document.body.classList.toggle('dark', darkModeEnabled)
+      }, [darkModeEnabled])
 
-  useEffect(() => {
-    localStorage.setItem(THEME, darkModeEnabled)
-    document.body.classList.toggle('dark', darkModeEnabled)
-  }, [darkModeEnabled])
+      return (
+        <button
+          {...props}
+          type="button"
+          className={styles.themeToggle}
+          aria-pressed={darkModeEnabled}
+          aria-label="Toggle dark theme"
+          onClick={() => setDarkModeEnabled(!darkModeEnabled)}
+        ></button>
+      )
+    }
 
-  return (
-    <button
-      {...props}
-      type="button"
-      className={styles.themeToggle}
-      aria-pressed={darkModeEnabled}
-      aria-label="Toggle dark theme"
-      onClick={() => setDarkModeEnabled(!darkModeEnabled)}
-    ></button>
-  )
-}
+export default ThemeToggleComponent
